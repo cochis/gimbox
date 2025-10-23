6ec73ca18d448019dd4b96f8e9cdbd286a1abe88
Oscar Ramirez <ing.oarrs@gmail.com>
2025-10-22 20:51:48 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+
+  currentYear = signal(new Date().getFullYear());
   title = 'landing';
 }
